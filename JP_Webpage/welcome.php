2ad7174b3229853dd3ffcbfcd686dfaf49a370e6
Paul Hubbard <phubbard67@gmail.com>
2018-02-07 21:59:06 -0800

    Version 0.003

--- JP_Webpage/welcome.php
+++ JP_Webpage/welcome.php
@@ -11,14 +11,19 @@
   <body style="background-color:#17a2b8;">
 
   <div class="py-5 text-center" style="background-color:#17a2b8; color:white; margin-top:3em;" >
+         <h2>Your current URL: <?php echo $_POST["url"]; ?></h2><br>
+	       <h2>Your new URL: </h2>
+  </div>
 
-        <h2>Your current URL: <?php echo $_POST["url"]; ?></h2><br>
-	<h2>Your new URL: </h2>
-    </div>
+  <div class="py-5 text-center" style="background-color:white; color:#17a2b8; margin-top:4em; padding:0 3em;" >
+          <h2>Now What?</h2>
+          <p>Now that you've gotten your token, just simply put the name of our website followed by a forward slash and your token, in place of the old URL, and presto! You now have a shorter URL</p>
+          <p>(ex. http://ourwebsite.com/token)</p>
+  </div>
+
     <!-- jQuery first, then Tether, then Bootstrap JS. -->
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
   </body>
 </html>
-
